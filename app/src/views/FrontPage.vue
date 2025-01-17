3bfd5748037f52b2994c9fc48d09785a115d8e4d
Laithanator <144480666+Laithanator@users.noreply.github.com>
2025-01-17 14:02:33 -0500

    graeh

---
+++ app/src/views/FrontPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <form action="submit">
+      <input type="text" />
+      <button></button>
+    </form>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
