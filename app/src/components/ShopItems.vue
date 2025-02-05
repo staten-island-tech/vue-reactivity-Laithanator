89e76c9a820afd205d723325c4ec8c422d387a41
Laithanator <144480666+Laithanator@users.noreply.github.com>
2025-02-05 14:02:09 -0500

    garehaehte

--- app/src/components/ShopItems.vue
+++ app/src/components/ShopItems.vue
@@ -1,6 +1,7 @@
 <template>
   <div
     class="shop-items w-[75vw] border-black border-[2px] bg-red-900 p-6 flex flex-wrap justify-center gap-6"
+    ref="itemsContainer"
   >
     <div
       v-for="item in items"
@@ -9,20 +10,29 @@
       class="card w-64 bg-white shadow-xl p-4 rounded-xl hover:shadow-2xl transition-shadow"
     >
       <figure class="w-full h-40 flex justify-center items-center">
-        <img :src="item.imageUrl" :alt="item.alt" class="w-full h-full object-cover rounded-lg" />
+        <img :src="item.imageUrl" :alt="item.alt" class="w-auto h-full object-cover rounded-lg" />
       </figure>
       <div class="card-body text-center">
         <h2 class="card-title text-lg font-bold text-gray-800">{{ item.name }}</h2>
         <p class="text-gray-600 text-sm">{{ item.description }}</p>
         <p class="text-lg font-semibold text-green-600 mt-2">${{ item.price }}</p>
-        <button class="btn btn-primary mt-4">Add to Cart</button>
+        <button
+          @click="toCart(item.name.replace(/\s+/g, '-').toLowerCase())"
+          class="btn btn-primary bg-green-600 hover:bg-green-700 mt-4 rounded-lg shadow-sm outline outline-2 outline-black"
+        >
+          Add to Cart
+        </button>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import { items } from './itemlist.js'
+const itemsContainer = ref(null)
+const cartContainer = ref(null)
+const toCart = (card) => {}
 </script>
 
 <style scoped></style>
--- app/src/components/ShoppingCart.vue
+++ app/src/components/ShoppingCart.vue
@@ -1,6 +1,7 @@
 <template>
   <div
     class="shop-header fixed right-0 top-0 h-[100vh] w-[25vw] border-black border-[2px] bg-orange-400 flex flex-col items-center text-center"
+    ref="cartContainer"
   >
     <h2 class="text-2xl font-bold mt-5 mb-5">Shopping Cart</h2>
   </div>
